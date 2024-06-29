7539f2787104e1699ceba751eb38c566e90eb9a8
zaklance <wosewick@gmail.com>
2024-06-29 10:08:49 -0400

    done

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,8 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        return json.loads(self.get_response_body())
